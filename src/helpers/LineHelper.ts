f69b40eca27d9045150f2f7ec5f458a21a9b86a1
fwx5618177 <fengwenxuan2006@126.com>
2024-04-18 17:28:40 +0800

    update helpers

--- src/helpers/LineHelper.ts
+++ src/helpers/LineHelper.ts
@@ -4,6 +4,17 @@ import { BaseHelper } from './BaseHelper';
 
 export class LineHelper extends BaseHelper implements HelperStrategy {
     override helperProcess(ctx: HelperCtx, node: TextNode): void {
+        const { value } = node;
+
+        if (!value) return;
+
+        /**
+         * TODO:
+         * 1. 如果只有 link，则处理 link
+         * 2. 如果包含 link 和 text，则提取 link
+         * 3. 根据不同的link，选择和处理不同的行为
+         */
+
         console.log(ctx, node);
     }
 }
